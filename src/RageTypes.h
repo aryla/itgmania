3ebac11d01a8c1fe69c6c198fb6e03d28c88b5d7
Arttu Ylä-Outinen <aryla@users.noreply.github.com>
2024-03-03 00:17:40 +0200

    RageTypes: Fix strict aliasing warning
    Changes FTOC in RageTypes.h to use a union instead of aliasing pointers
    to fix a compiler warning for every file that includes RageTypes.h.

--- src/RageTypes.h
+++ src/RageTypes.h
@@ -291,13 +291,14 @@ inline unsigned char FTOC(float a)
 	* should be converted to 1 is roughly(1 / 256 - 0.00001); if we don't
 	* truncate, values up to (1/256 + 0.5) will be converted to 1, which is
 	* wrong. */
-	double base = double(a * 256.f - 0.5f);
+	union { double d; int i; } base;
+	base.d = double(a * 256.f - 0.5f);
 
 	/* INT_MANTISSA is chosen such that, when added to a sufficiently small
 	* double, the mantissa bits of that double can be reinterpreted as that
 	* number rounded to an integer. This is done to improve performance. */
-	base += INT_MANTISSA;
-	int ret = reinterpret_cast<int&>(base);
+	base.d += INT_MANTISSA;
+	int ret = base.i;
 
 	/* Benchmarking shows that clamping here, as integers, is much faster than clamping
 	* before the conversion, as floats. */
